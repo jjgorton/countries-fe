f563a0a4c4a7e77d4c4170765cca8df248938222
jjgorton <jjgorton@gmail.com>
2020-11-11 21:54:07 -0500

    trying out various map libraries

--- countries-fe/src/components/map/Map.jsx
+++ countries-fe/src/components/map/Map.jsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import ReactMapGL from 'react-map-gl';
 import Plot from 'react-plotly.js';
 
 const Map = () => {
+    const [viewport, setViewport] = useState({
+        width: 400,
+        height: 400,
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8,
+    });
+
     return (
         <div>
-            <h2>Maps</h2>
-
-            <Plot
-                data={[
-                    {
-                        type: 'choropleth',
-                        locationmode: 'country names',
-
-                        autocolorscale: true,
-                    },
-                ]}
-            />
+            <h2
+                onClick={(e) => {
+                    console.log('clicked');
+                }}>
+                Maps
+            </h2>
+            <ReactMapGL
+                {...viewport}
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+                onViewportChange={(viewport) => {
+                    setViewport({ viewport });
+                }}>
+                <div></div>
+            </ReactMapGL>
         </div>
     );
 };
