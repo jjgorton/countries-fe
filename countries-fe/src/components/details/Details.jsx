9cf265944cee4cd55cad5e77398c83dea7683bfc
jjgorton <jjgorton@gmail.com>
2020-11-17 16:58:02 -0500

    styled details to have a flip button and animations

--- countries-fe/src/components/details/Details.jsx
+++ countries-fe/src/components/details/Details.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Draggable } from 'react-beautiful-dnd';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
 import './details.scss';
 
 const Details = ({ country, index, id }) => {
+    const [showInfo, setShowInfo] = useState(false);
+
     return (
         <Draggable draggableId={id} index={index}>
             {(provided) => (
                 <div
-                    className='details'
+                    className='details-container'
                     {...provided.draggableProps}
                     ref={provided.innerRef}
                     {...provided.dragHandleProps}>
-                    <h1>{country.name}</h1>
-                    <p>Capital: {country.capital}</p>
-                    <p>Population: {country.population}</p>
+                    <div className={`details ${showInfo && 'info'}`}>
+                        {!showInfo && <h1>{country.name}</h1>}
+                        {showInfo && (
+                            <div>
+                                <p>Capital: {country.capital}</p>
+                                <p>Population: {country.population}</p>
+                            </div>
+                        )}
+                    </div>
+                    <FontAwesomeIcon
+                        icon={faSyncAlt}
+                        className='flip-icon'
+                        onClick={() => setShowInfo(!showInfo)}
+                    />
                 </div>
             )}
         </Draggable>
