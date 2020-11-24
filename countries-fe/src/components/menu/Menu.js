56c366aec0401186045be5133c24f20862ef35a6
jjgorton <jjgorton@gmail.com>
2020-11-23 19:17:37 -0500

    styled download button

--- countries-fe/src/components/menu/Menu.js
+++ countries-fe/src/components/menu/Menu.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import DownloadLink from 'react-download-link';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faChevronRight,
+    faChevronLeft,
+    faUpload,
+    faDownload,
+} from '@fortawesome/free-solid-svg-icons';
 
 import './menu.scss';
 
 const Menu = ({ history, setHistory, itinerary, setItinerary }) => {
+    const [showMenu, setShowMenu] = useState(false);
+    const [hover, setHover] = useState(false);
+
     const handleUpload = (e) => {
         const uploadedFile = e.target.files[0];
 
@@ -24,14 +35,41 @@ const Menu = ({ history, setHistory, itinerary, setItinerary }) => {
     };
 
     return (
-        <div className='menu'>
-            <DownloadLink
-                className='download'
-                label='Save'
-                filename='myfile.txt'
-                exportFile={() => JSON.stringify({ history, itinerary })}
-            />
-            <input type='file' onChange={handleUpload} accept='.txt' />
+        <div className={`menu-container ${showMenu && 'hide-menu'}`}>
+            <div className='title'>
+                <h1>Menu</h1>
+                <FontAwesomeIcon
+                    icon={showMenu ? faChevronRight : faChevronLeft}
+                    className='slide-icon'
+                    onClick={() => setShowMenu(!showMenu)}
+                />
+            </div>
+            <div className='menu'>
+                <div
+                    className='download-container'
+                    onMouseEnter={() => setHover(true)}
+                    onMouseLeave={() => setHover(false)}>
+                    <DownloadLink
+                        className='link'
+                        label=''
+                        filename='myfile.txt'
+                        exportFile={() =>
+                            JSON.stringify({ history, itinerary })
+                        }
+                    />
+                    <div className={`download ${hover && 'fakeHover'}`}>
+                        <FontAwesomeIcon
+                            icon={faDownload}
+                            className='load-icon'
+                        />
+                    </div>
+                </div>
+                <form className='upload'>
+                    <FontAwesomeIcon icon={faUpload} className='load-icon' />
+                    <input type='file' onInput={handleUpload} accept='.txt' />
+                    <button>Load</button>
+                </form>
+            </div>
         </div>
     );
 };
