f939e2f868cfeb3bac73bdad8e074c9411306f24
jjgorton <jjgorton@gmail.com>
2021-12-28 22:44:52 -0500

    built TSP utilities and tests

--- countries-fe/src/utils/buildGraph.ts
+++ countries-fe/src/utils/buildGraph.ts
@@ -14,19 +14,26 @@ function buildGraph(allCountries: Country[], itinerary: Itinerary[]) {
     const graph: any = {};
 
     for (let i = 0; i < itinerary.length; i++) {
-        const cur: number | string = itinerary[i].countryIndex;
-        const lat1 = allCountries[cur].latlng[0];
-        const lon1 = allCountries[cur].latlng[1];
+        const cur: Itinerary = itinerary[i];
+        const curIndex: number = Number(cur.id.split('-')[0]);
+        const lat1 = allCountries[curIndex].latlng[0];
+        const lon1 = allCountries[curIndex].latlng[1];
 
-        graph[cur] = {};
+        graph[cur.id] = {};
 
         for (let j = 0; j < itinerary.length; j++) {
             if (j !== i) {
-                const next = itinerary[j].countryIndex;
-                const lat2 = allCountries[next].latlng[0];
-                const lon2 = allCountries[next].latlng[1];
+                const next: Itinerary = itinerary[j];
+                const nextIndex: number = Number(next.id.split('-')[0]);
+                const lat2 = allCountries[nextIndex].latlng[0];
+                const lon2 = allCountries[nextIndex].latlng[1];
 
-                graph[cur][next] = distanceOnSphere(lat1, lat2, lon1, lon2);
+                graph[cur.id][next.id] = distanceOnSphere(
+                    lat1,
+                    lat2,
+                    lon1,
+                    lon2
+                );
             }
         }
     }
@@ -34,6 +41,54 @@ function buildGraph(allCountries: Country[], itinerary: Itinerary[]) {
     return graph;
 }
 
-// //return new itinerary array of objects (sorted)
-// return [];
-export { buildGraph };
+interface Node {
+    path: string[];
+    distance: number;
+    visited: Set<string>;
+}
+
+function findShortestPath(allCountries: Country[], itinerary: Itinerary[]) {
+    const graph = buildGraph(allCountries, itinerary);
+
+    let shortestPath: string[] = [];
+    let shortestDistance: number = Infinity;
+
+    for (let start of itinerary) {
+        const stack: Node[] = [
+            { path: [start.id], distance: 0, visited: new Set() },
+        ];
+
+        while (stack.length) {
+            const cur: Node = stack.pop();
+            cur.visited.add(cur.path[cur.path.length - 1]);
+
+            if (
+                cur.path.length === itinerary.length &&
+                cur.distance < shortestDistance
+            ) {
+                shortestDistance = cur.distance;
+                shortestPath = cur.path;
+            }
+
+            const connections = graph[cur.path[cur.path.length - 1]];
+
+            for (let i in connections) {
+                if (!cur.visited.has(i)) {
+                    const copyCur: Node = {
+                        path: [...cur.path, i],
+                        distance: cur.distance + connections[i],
+                        visited: new Set<string>(cur.visited),
+                    };
+
+                    stack.push(copyCur);
+                }
+            }
+        }
+    }
+
+    return shortestPath.map((stringId) => {
+        return { countryIndex: Number(stringId.split('-')[0]), id: stringId };
+    });
+}
+
+export { buildGraph, findShortestPath };
